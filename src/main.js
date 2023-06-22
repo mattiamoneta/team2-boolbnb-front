5234a0a12e62331b595719d29a080b804803d801
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-22 11:19:29 +0200

    Fixed Fonts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './styles/style.scss'
 import App from './App.vue'
+import '@fontsource-variable/lora';
+import '@fontsource-variable/montserrat';
 
 import { router } from './router';
 import 'bootstrap';
