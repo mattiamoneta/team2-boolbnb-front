53cee12708686decfb7489d0e49b4229be9f0150
Irene Brignoli <ire.brignoli@gmail.com>
2023-06-22 11:30:18 +0200

    componenti creati

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<script>
+import { store } from "../store";
+export default {
+  name: "TheHome",
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
