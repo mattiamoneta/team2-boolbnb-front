1285c25d98be739c3294d75b75e8ad344f153214
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-22 11:29:20 +0200

    Navbar Upd 1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,7 @@
 <template>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand" href="#">Boolbnb</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,32 +23,25 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
                     </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                    </li>
+   
 
-                    <button class="my-3 btn ms-btn-outline-primary rounded-4 btn-sm w-100" href="#">
-                        <i class="fa-solid fa-right-from-bracket me-2"></i>
-                            Login
-                    </button>
+                    <li class="nav-item dropstart">
+                        <a id="navbarDropdown" class="nav-link user-dropdown fw-bold px-3 py-1"
+                        data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" v-pre>
+                            <span class="align-middle">Area Utente</span>
+                            <div class="user-icon ms-bg-dark align-middle">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                        </a>
+
+                    </li>
                 </ul>
 
           
@@ -56,6 +49,32 @@
         </div>
     </nav>
     <!-- End Navbar -->
+
+    <!-- Offcanvas -->
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header">
+        <h5 class="offcanvas-title font-secondary" id="offcanvasExampleLabel">Menu</h5>
+        <div class="offcanvas-right d-flex align-items-center">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="list-unstyled">
+        
+                <li>
+                    <!-- <button class="my-3 btn ms-btn-outline-primary rounded-4 btn-sm w-100" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    <i class="fa-solid fa-right-from-bracket me-2"></i>
+                    {{ __('Logout') }}
+                    </button> -->
+                </li>
+            </ul>
+
+        
+        </div>
+    </div>
+  <!-- End Offcanvas -->
 </template>
 
 <style scoped lang="scss">
