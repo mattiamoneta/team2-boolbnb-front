e5c63938b521cc6ca0957901dd9ce2c56c0bc8ea
nicolo fossati <nicofossa01@gmail.com>
2023-06-23 08:41:47 +0200

    lat-lon

--- src/components/MainSearchbar.vue
+++ src/components/MainSearchbar.vue
@@ -1,11 +1,59 @@
 <script>
 import { store } from "../store";
+import axios from 'axios';
 export default {
   name: "MainSearchbar",
   data() {
     return {
       store,
+      address: '',
+      suggestions: [],
     };
+  },
+  methods: {
+    handleInput() {
+      if (this.address.length > 2) {
+        fetch(`https://api.tomtom.com/search/2/search/${encodeURIComponent(this.address)}.json?key=ZPskuspkrrcmchd9ut4twltuw96h5bWH&language=it-IT`)
+          .then(response => response.json())
+          .then(data => {
+            this.suggestions = data.results;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      } else {
+        this.suggestions = [];
+      }
+    },
+    selectAddress(suggestion) {
+      this.address = suggestion.address.freeformAddress;
+      this.suggestions = [];
+    }, submitForm() {
+      const selectedAddress = this.address;
+      const apiKey = "ZPskuspkrrcmchd9ut4twltuw96h5bWH";
+
+      fetch(`https://api.tomtom.com/search/2/geocode/${encodeURIComponent(selectedAddress)}.json?key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data && data.results && data.results.length > 0) {
+            const latitude = data.results[0].position.lat;
+            const longitude = data.results[0].position.lon;
+
+            axios.get('http://localhost:8000/api/apartments')
+              .then(response => {
+                const apartments = response.data.results; //tutti gli appartamenti del DB
+                console.log(apartments);
+                console.log("lat: " + latitude + ",lon: " + longitude);
+              })
+              .catch(error => {
+                console.error(error);
+              });
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
   },
 };
 </script>
@@ -13,22 +61,20 @@ export default {
 <template>
   <!-- searchbar -->
   <div class="position-relative">
-    <div
-      class="container p-3 rounded-3 bg-white position-absolute top-0 start-50 translate-middle"
-    >
+    <div class="container p-3 rounded-3 bg-white position-absolute top-0 start-50 translate-middle">
       <div class="border p-2 rounded ps-3">
-        <form
-          class="d-flex justify-content-center align-items-center"
-          role="search"
-        >
+        <form class="d-flex justify-content-center align-items-center" role="search" @submit.prevent="submitForm">
           <div>
             <i class="fa-solid fa-magnifying-glass ms_light_gray_text"></i>
           </div>
-          <input
-            class="form-control me-2 border border-0"
-            type="search"
-            placeholder="Cerca una località"
-          />
+          <div>
+            <input class="form-control me-2 border border-0" type="search" placeholder="Cerca una località"
+              v-model="address" @input="handleInput" />
+            <ul v-if="suggestions.length">
+              <li v-for="suggestion in suggestions" :key="suggestion.id" @click="selectAddress(suggestion)">{{
+                suggestion.address.freeformAddress }}</li>
+            </ul>
+          </div>
           <button class="btn ms-btn-outline-primary" type="submit">
             Cerca
           </button>
