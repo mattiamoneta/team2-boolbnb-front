dc6176f3986efef8a2a770f081579e4485367e2d
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-23 11:08:00 +0200

    Edit Results Page Size

--- src/pages/ApartmentsList.vue
+++ src/pages/ApartmentsList.vue
@@ -46,7 +46,7 @@ export default {
    
       <div class="row">
       <!-- Results -->
-      <div class="col-12 col-lg-7">
+      <div class="col-12 col-lg-5">
         <div class="fixed-box pe-4 py-3">
         <ApartmentResultCard v-for="i in 5" />
         </div>
@@ -54,7 +54,7 @@ export default {
       <!-- End Results -->
 
       <!-- Map -->
-      <div class="col-5 max-fixed d-none d-lg-block">
+      <div class="col-7 max-fixed d-none d-lg-block">
           
         <div class="card  d-block rounded-4 overflow-hidden border-1 h-100">
           <iframe class="map-frame" width="100%" height="100%" frameBorder="0" src="https://www.openstreetmap.org/export/embed.html?bbox=9.02132034301758%2C45.41267141842337%2C9.357776641845705%2C45.51693278828882&amp;layer=mapnik" style="border: 1px solid black"></iframe>
