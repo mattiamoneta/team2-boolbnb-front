747855570e9891d9d5154e6a4f803fda7ec06ad4
nicolo fossati <nicofossa01@gmail.com>
2023-06-27 14:43:02 +0200

    merge con chiave

--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ import { reactive } from "vue";
 
 export const store = reactive({
   baseUrl: "http://localhost:8000",
-  apiKey: "8WZyDOoy7ND8eeoGrJeUfxODvJmK39q7",
+  apiKey: "ZPskuspkrrcmchd9ut4twltuw96h5bWH",
   queryAddress: "",
   querySuggestionsLimit: 6,
   querySuggestions: [],
   radius: 20,
   retApartmnets: {}
-  
-  
+
+
 
   //dati per filtraggio
 
