f089ff41dc4d064a2c4df538a06c11dc8ef48159
Irene Brignoli <ire.brignoli@gmail.com>
2023-06-27 11:19:47 +0200

    aggiornamento

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -17,15 +17,31 @@ export default {
 
 <template>
   <main :class="scrollPos > 230 ? 'navbar-top-fix' : ''">
-    <section id="cover-image"></section>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-      <path
-        fill="#ffffff"
-        fill-opacity="1"
-        d="M0,128L30,138.7C60,149,120,171,180,154.7C240,139,300,85,360,64C420,43,480,53,540,85.3C600,117,660,171,720,170.7C780,171,840,117,900,85.3C960,53,1020,43,1080,69.3C1140,96,1200,160,1260,181.3C1320,203,1380,181,1410,170.7L1440,160L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
-        class="position-relative"
-      ></path>
-    </svg>
+    <section id="cover-image" class="position-relative">
+      <div class="container h-100">
+        <div
+          class="d-flex align-items-center justify-content-center flex-column h-100 text-center"
+        >
+          <h1 class="text-white mb-4 display-3">
+            Goditi una vacanza <br />
+            indimenticabile con Boolbnb!
+          </h1>
+          <p class="text-white">
+            L' obiettivo Boolbnb è ridefinire il concetto di ospitalità e
+            offrire soggiorni di qualità in tutto il mondo.
+          </p>
+        </div>
+      </div>
+      <div class="waves">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#ffffff"
+            fill-opacity="1"
+            d="M0,96L80,106.7C160,117,320,139,480,170.7C640,203,800,245,960,240C1120,235,1280,181,1360,154.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -33,9 +49,17 @@ export default {
 @use "../styles/partials/colors" as *;
 
 #cover-image {
-  height: 500px;
+  height: 600px;
   background-image: url("https://www.aveliving.com/AVE/media/Property_Images/Florham%20Park/hero/flor-apt-living-(2)-hero.jpg?ext=.jpg");
   background-position: center;
+  opacity: 90%;
   background-size: cover;
+
+  .waves {
+    position: absolute;
+    top: 20rem;
+    right: 0;
+    width: 100%;
+  }
 }
 </style>
