45556e419592d80f54e46bae3d613a61991bc043
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-27 17:45:02 +0200

    Homepage Responsive

--- src/components/AppFeatureSection.vue
+++ src/components/AppFeatureSection.vue
@@ -13,16 +13,16 @@
 <template>
     <div class="container py-5">
         <div class="row align-items-center justify-content-center">
-                <div class="col-4 ms_border_right_primary">
+                <div class="col-lg-4 col-12 ms_border_right_primary text-center text-lg-start">
                     <h2>
                         Il tuo rifugio esclusivo, <span class="ms_text_main_darker font-primary"><br/>solo un clic</span> di distanza.
                     </h2>
                 </div>
 
-                <div class="col-8">
+                <div class="col-lg-8 col-12">
                     <div class="row">
 
-                        <div class="col-5 offset-2 text-end py-5">
+                        <div class="col-lg-5 col-12 text-center offset-lg-2 text-lg-end py-5">
                             <div class="feature-icon">
                                 <i class="fa-solid fa-feather fa-2x ms_text_main_darker"></i>
                                 <h6 class="text-uppercase font-semibold font-secondary letter_spacing mb-4 xmedium mt-4 mb-3">Selezione esclusiva</h6>
@@ -30,7 +30,7 @@
                             </div>
                         </div>
 
-                        <div class="col-5 text-end py-5">
+                        <div class="col-lg-5 col-12 text-center text-lg-end py-5">
                             <div class="feature-icon">
                                 <i class="fa-solid fa-face-smile fa-2x ms_text_main_darker"></i>
                                 <h6 class="text-uppercase font-semibold font-secondary letter_spacing mb-4 xmedium mt-4 mb-3">Esperienza senza stress</h6>
